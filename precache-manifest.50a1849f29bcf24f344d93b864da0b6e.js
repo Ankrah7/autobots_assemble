6b751d937901c6235e92118c17bd61dbf9dcd606
Ernest <etankrah777@gmail.com>
2022-07-25 15:20:22 -0400

    Updates

---
+++ precache-manifest.50a1849f29bcf24f344d93b864da0b6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0853d3498a361fab3c5d",
+    "url": "/autobots_assemble/static/css/main.22519e3a.chunk.css"
+  },
+  {
+    "revision": "0853d3498a361fab3c5d",
+    "url": "/autobots_assemble/static/js/main.c4feda00.chunk.js"
+  },
+  {
+    "revision": "595525910b4ded05064c",
+    "url": "/autobots_assemble/static/js/runtime~main.8803f043.js"
+  },
+  {
+    "revision": "9c9f9f8783ad4c04d44f",
+    "url": "/autobots_assemble/static/css/2.39c4c729.chunk.css"
+  },
+  {
+    "revision": "9c9f9f8783ad4c04d44f",
+    "url": "/autobots_assemble/static/js/2.78e1aefb.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/autobots_assemble/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/autobots_assemble/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "6d17a43004d9b00aa81c952bab0ba2c1",
+    "url": "/autobots_assemble/index.html"
+  }
+];
